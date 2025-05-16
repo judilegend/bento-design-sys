a403fc50de6ef0755aa2e94e96e08b6471ecf5e9
judilegend <judilegend2@gmail.com>
2025-05-16 17:25:30 +0300

    feat: creer la carte flower

---
+++ src/components/cards/FollowersCard.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import BentoCard from "../BentoCard";
+
+/**
+ * Carte "Grow followers with non-stop content"
+ */
+export default function FollowersCard() {
+  return (
+    <BentoCard
+      className="flex flex-row justify-center items-center gap-2.5"
+      gridArea="5 / 5 / 7 / 9"
+      bgColor="bg-purple"
+    >
+      <div className="relative w-[150px] h-[100px] ml-5">
+        <Image
+          src="/images/illustration-grow-followers.webp"
+          alt="Grow followers illustration"
+          fill
+          sizes="150px"
+          className="object-contain"
+        />
+      </div>
+      <p className="mr-5 text-2xl text-white text-left">
+        Grow followers with non-stop content
+      </p>
+    </BentoCard>
+  );
+}
